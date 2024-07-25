3682df102612db0f559654f8c857e0cc9e9d110f
Savage9193 <tyagishahvez551@gmail.com>
2024-07-25 18:25:50 +0530

    update

---
+++ index.js
@@ -0,0 +1,8 @@
+let daynight=document.querySelector(".daynight");
+        let wrapper=document.querySelector("#wrapper");
+
+
+
+    daynight.addEventListener("click",()=>{
+    wrapper.classList.toggle("night");
+})
